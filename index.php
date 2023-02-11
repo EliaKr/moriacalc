9ecec24205f5b665fc9272d8727415f1e45056a7
Elias <33204933+EliaKr@users.noreply.github.com>
2023-02-11 13:44:57 +0000

    Removed excess code from index.php

--- index.php
+++ index.php
@@ -1,20 +1,11 @@
 <style>
-h2 {text-align: center; font-family:verdana; color: white; font-size: 18px;}
 a {text-align: center; font-family:verdana; font-size: 18px; font-weight: bold; line-height: 40px;}
 h3 {text-align: center; font-family:verdana; font-weight: bold; color: white;}
-p {text-align: center; font-family:verdana; color: white;}
-p2 {text-align: center; font-family:verdana; color: white; font-size: 12px;}
-b {text-align: center; font-family:verdana; color: white; line-height: 40px;}
-span {font-weight: bold; font-family:verdana; color: white; font-size: 24px;}
 div {text-align: center; vertical-align: middle; padding: 20% 0;}
-form {overflow: hidden;}
-input {text-align: right; font-weight: bold; font-size: 16px;}
-
 body {background-color: 1b1b1b;}
 </style>
 <head>
   <title>Υπολογισμός Μορίων Πανελλαδικών 2023</title>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 
 
